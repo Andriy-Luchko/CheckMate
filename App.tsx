0b8aef2744211ded65b60c339e00b1408a8f640e
androozhang <zandrew129@gmail.com>
2024-04-20 17:42:24 -0400

    downloaded native stack

--- App.tsx
+++ App.tsx
@@ -25,6 +25,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 type SectionProps = PropsWithChildren<{
   title: string;
